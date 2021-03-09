f9694980c2972f557333f29ae59c3d41c61465aa
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-08 23:57:05 -0600

    updated database.php

--- model/database.php
+++ model/database.php
@@ -1,5 +1,5 @@
 <?php
-    $dsn = 'eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $dsn = 'y15ma8233qeiei71';
     $username = 'a36r0n5p9xrvfqpe';
     $password = 'puilsn09m55nmfjl';
 
