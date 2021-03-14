e7cc54f4b045ac735d8c01637c343427b8f131f1
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-13 19:22:57 -0600

    testing resizing the table

--- view/vehicle_list.php
+++ view/vehicle_list.php
@@ -46,77 +46,78 @@
     </div>
 </form>
 
-<table class = "table table-hover" id = "tab">
-    <tr>
-        <th>Year</th>
-        <th>Make</th>
-        <th>Model</th>
-        <th>Type</th>
-        <th>Class</th>
-        <th>Price</th>
-    </tr>
-    <?php
-        $select_make;
-        $select_type;
-        $select_class;
-        $select_sort;
-        $query = "SELECT year, makeID, model, typeID, classID, price FROM vehicles WHERE ";
+<div id = "scroll">
+    <table class = "table table-hover" id = "tab">
+        <tr>
+            <th>Year</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Type</th>
+            <th>Class</th>
+            <th>Price</th>
+        </tr>
+        <?php
+            $select_make;
+            $select_type;
+            $select_class;
+            $select_sort;
+            $query = "SELECT year, makeID, model, typeID, classID, price FROM vehicles WHERE ";
 
-        if (isset($_POST["select_make"]) && $_POST["select_make"] != "all_makes")
-        {
-            $select_make = $_POST["select_make"];
-            $query .= "makeID = $select_make AND ";
-        }
-        if (isset($_POST["select_type"]) && $_POST["select_type"] != "all_types")
-        {
-            $select_type = $_POST["select_type"];
-            $query .= "typeID = $select_type AND ";
-        }
-        if (isset($_POST["select_class"]) && $_POST["select_class"] != "all_classes")
-        {
-            $select_class = $_POST["select_class"];
-            $query .= "classID = $select_class AND ";
-        }
-        $query .= "true ";
-        if (isset($_POST["sort"]))
-        {
-            $select_sort = $_POST["sort"];
-            $query .= "ORDER BY $select_sort DESC";
-        }
+            if (isset($_POST["select_make"]) && $_POST["select_make"] != "all_makes")
+            {
+                $select_make = $_POST["select_make"];
+                $query .= "makeID = $select_make AND ";
+            }
+            if (isset($_POST["select_type"]) && $_POST["select_type"] != "all_types")
+            {
+                $select_type = $_POST["select_type"];
+                $query .= "typeID = $select_type AND ";
+            }
+            if (isset($_POST["select_class"]) && $_POST["select_class"] != "all_classes")
+            {
+                $select_class = $_POST["select_class"];
+                $query .= "classID = $select_class AND ";
+            }
+            $query .= "true ";
+            if (isset($_POST["sort"]))
+            {
+                $select_sort = $_POST["sort"];
+                $query .= "ORDER BY $select_sort DESC";
+            }
 
-        if (!isset($_POST["select_make"]) && !isset($_POST["select_type"]) && !isset($_POST["select_class"]) && !isset($_POST["select_sort"]))
-        {
-            $query = "SELECT year, makeID, model, typeID, classID, price FROM vehicles ORDER BY price DESC";
-        }
-        
-        $results = fetch_all($query, $db);
-        foreach ($results as $result) :
-            $make_id = $result["makeID"];
-            $type_id = $result["typeID"];
-            $class_id = $result["classID"];
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($result["year"]) . "</td>";
+            if (!isset($_POST["select_make"]) && !isset($_POST["select_type"]) && !isset($_POST["select_class"]) && !isset($_POST["select_sort"]))
+            {
+                $query = "SELECT year, makeID, model, typeID, classID, price FROM vehicles ORDER BY price DESC";
+            }
+            
+            $results = fetch_all($query, $db);
+            foreach ($results as $result) :
+                $make_id = $result["makeID"];
+                $type_id = $result["typeID"];
+                $class_id = $result["classID"];
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($result["year"]) . "</td>";
 
-            $query1 = "SELECT makeName FROM makes WHERE makeID = $make_id";
-            $result1 = fetch_one($query1, $db);
-            echo "<td>" . htmlspecialchars($result1["makeName"]) . "</td>";
+                $query1 = "SELECT makeName FROM makes WHERE makeID = $make_id";
+                $result1 = fetch_one($query1, $db);
+                echo "<td>" . htmlspecialchars($result1["makeName"]) . "</td>";
 
-            echo "<td>" . htmlspecialchars($result["model"]) . "</td>";
+                echo "<td>" . htmlspecialchars($result["model"]) . "</td>";
 
-            $query2 = "SELECT typeName FROM types WHERE typeID = $type_id";
-            $result2 = fetch_one($query2, $db);
-            echo "<td>" . htmlspecialchars($result2["typeName"]) . "</td>";
+                $query2 = "SELECT typeName FROM types WHERE typeID = $type_id";
+                $result2 = fetch_one($query2, $db);
+                echo "<td>" . htmlspecialchars($result2["typeName"]) . "</td>";
 
-            $query3 = "SELECT className FROM classes WHERE classID = $class_id";
-            $result3 = fetch_one($query3, $db);
-            echo "<td>" . htmlspecialchars($result3["className"]) . "</td>";
+                $query3 = "SELECT className FROM classes WHERE classID = $class_id";
+                $result3 = fetch_one($query3, $db);
+                echo "<td>" . htmlspecialchars($result3["className"]) . "</td>";
 
-            echo "<td>$" . number_format(htmlspecialchars($result["price"])) . ".00</td>";
-            echo "</tr>";
-        endforeach;
-    ?>
-</table>
-
+                echo "<td>$" . number_format(htmlspecialchars($result["price"])) . ".00</td>";
+                echo "</tr>";
+            endforeach;
+        ?>
+    </table>
+</div>
 <?php
     include("view/footer.php");
 ?>
