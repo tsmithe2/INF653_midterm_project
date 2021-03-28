cce797cc54d5a61fb868484242ff6f9c20d269a0
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-28 18:50:08 -0500

    testing

--- view/is_registered.php
+++ view/is_registered.php
@@ -3,6 +3,6 @@
     include("view/header.php");
     $_SESSION["firstname"] = $_POST["first_name"];
     echo "<h4>Thank you for registering, " . $_SESSION["firstname"] . "!</h4>";
-    echo "<a href = 'vehicle_list.php'>Click here</a> to view our vehicle list.";
+    echo "<a href = '../index.php'>Click here</a> to view our vehicle list.";
     include("view/footer.php");
 ?>
