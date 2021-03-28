93ba4852a0f15bb696702236f7b6750676110d85
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-28 11:49:07 -0500

    testing

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+    // Start session management with a persistent cookie 
+    $lifetime = 60 * 60 * 24 * 14; // 2 weeks in seconds 
+    session_set_cookie_params($lifetime, '/'); 
+    session_start();
+
     require("model/database.php");
+
+    if (isset($_POST["name"]))
+    {
+        echo "hi";
+    }
     include("view/vehicle_list.php");
 ?>
