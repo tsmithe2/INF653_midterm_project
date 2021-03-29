7ae3ba54b4417ab3444b05269ff62979ed94aea4
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-28 20:01:29 -0500

    testing

--- view/header.php
+++ view/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset = "UTF-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type = "text/css" href="css/main.css" />
+    <link rel = "stylesheet" type = "text/css" href = "css/main.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Zippy Used Autos</title>
 </head>
@@ -13,7 +13,10 @@
             session_start();
             if (!isset($_SESSION["firstname"]))
             {
-                echo "<a href = 'view/register.php'>Register</a>";
+                //echo "<a href = 'view/register.php'>Register</a>";
+                echo "<form action = '../index.php' method = 'POST'>
+                <input type = 'submit' name = 'register' value = 'Register' />
+                </form>";
             }
             else
             {
