eeed0f21223acd524a5c86560d72dde7fb2da999
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-28 21:41:08 -0500

    styling

--- view/register.php
+++ view/register.php
@@ -3,11 +3,13 @@
     $_SESSION["isBusy"] = true;
     include("header.php");
 ?>
-<form action = "../index.php" method = "GET">
-    <label for = "name">Please enter your first name:</label>
-    <input type = "text" name = "first_name" required />
+
+<form action = "../index.php" method = "GET" id = "reg_form">
+    <label for = "name">Please enter your first name:</label><br>
+    <input type = "text" name = "first_name" required /><br>
     <input type = "submit" name = 'is_reg' value = "Register" />
 </form>
+
 <?php
     include("footer.php");
 ?>
