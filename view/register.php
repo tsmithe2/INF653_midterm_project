edcd02d27280ddf8b52897a79b8fb2f1fc9adb63
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-28 20:48:42 -0500

    testing

--- view/register.php
+++ view/register.php
@@ -2,7 +2,7 @@
     session_start();
     include("header.php");
 ?>
-<form action = "index.php" method = "GET">
+<form action = "../index.php" method = "GET">
     <label for = "name">Please enter your first name:</label>
     <input type = "text" name = "first_name" required />
     <input type = "submit" name = 'is_reg' value = "Register" />
