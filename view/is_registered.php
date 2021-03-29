b0b13cb05af207c86cdfefc2af017f1f0b405914
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-28 22:32:36 -0500

    styling

--- view/is_registered.php
+++ view/is_registered.php
@@ -3,7 +3,7 @@
     $_SESSION["isBusy"] = true;
     include("view/header.php");
     $_SESSION["firstname"] = $_GET["first_name"];
-    echo "<h4>Thank you for registering, " . $_SESSION["firstname"] . "!</h4>";
-    echo "<a href = '../index.php'>Click here</a> to view our vehicle list.";
+    echo "<div id = 'reg_form'><h4>Thank you for registering, " . $_SESSION["firstname"] . "!</h4>";
+    echo "<a href = '../index.php'>Click here</a> to view our vehicle list.</div>";
     include("view/footer.php");
 ?>
--- view/logout.php
+++ view/logout.php
@@ -2,8 +2,8 @@
     session_start();
     $_SESSION["isBusy"] = true;
     include("view/header.php");
-    echo "<h4>Thank you for signing out, " . $_SESSION["firstname"] . ".</h4>";
-    echo "<a href = '../index.php'>Click here</a> to view our vehicle list.";
+    echo "<div id = 'reg_form'><h4>Thank you for signing out, " . $_SESSION["firstname"] . ".</h4>";
+    echo "<a href = '../index.php'>Click here</a> to view our vehicle list.</div>";
     include("view/footer.php");
     $_SESSION = array();
     session_destroy();
