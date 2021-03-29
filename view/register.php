5995a87c4d0a05e07f8dd5c50836501a4d6fdeeb
Thomas Smithey <thomas_smithey@yahoo.com>
2021-03-28 22:24:43 -0500

    styling

--- view/register.php
+++ view/register.php
@@ -7,7 +7,7 @@
 <form action = "../index.php" method = "GET" id = "reg_form">
     <label for = "name">Please enter your first name:</label><br>
     <input type = "text" name = "first_name" id = "reg_name" required /><br>
-    <input type = "submit" name = 'is_reg' value = "Register" id = "reg_subm" />
+    <input type = "submit" name = 'is_reg' value = "Register" id = "reg_subm" class = "btn btn-success"/>
 </form>
 
 <?php
