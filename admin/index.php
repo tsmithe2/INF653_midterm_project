0b64188dd9b1c487bc12d4a2d718d3e59b79e0cb
Thomas Smithey <thomas_smithey@yahoo.com>
2021-04-09 17:34:14 -0500

    testing

--- admin/index.php
+++ admin/index.php
@@ -11,8 +11,8 @@
     $password = $_POST["password"];
     $confirm_password = $_POST["confirm_password"];
 
-    $query = "SELECT * FROM administrators";
-    $result = fetch_all($query);
+    $query = "SELECT username FROM administrators WHERE username = 'Zippy123'";
+    $result = fetch_one($query);
     
     if ($result == "")
     {
