f1d9618aae097f5abaee6125ee84c7170457fb7e
Thomas Smithey <thomas_smithey@yahoo.com>
2021-04-09 16:49:46 -0500

    testing

--- admin/view/login.php
+++ admin/view/login.php
@@ -4,13 +4,13 @@
 
 <div id = "nput_form">
     <h4>Please fill in your credentials to login.</h4>
-    
+
     <form action = "index.php" method = "POST">
         <label for = "username">Username:</label><br>
-        <input type = "text" name = "username" id = "nput" class = "form-control" required><br>
+        <input type = "text" name = "username" id = "nput" class = "form-control" required>
 
         <label for = "password">Password:</label><br>
-        <input type = "password" name = "password" id = "nput" class = "form-control" required><br>
+        <input type = "password" name = "password" id = "nput" class = "form-control" required>
 
         <input type = "submit" value = "Sign in" id = "nput_s" class="btn btn-outline-primary">
     </form>
