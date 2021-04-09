2a72d6f99000c489733a6182fd72e9720548582f
Thomas Smithey <thomas_smithey@yahoo.com>
2021-04-09 16:40:07 -0500

    testing

--- admin/view/login.php
+++ admin/view/login.php
@@ -1,4 +1,18 @@
 <?php
     include("header.php");
+?>
+
+<h4>Please fill in your credentials to login.</h4>
+<form action = "index.php" method = "POST">
+    <label for = "username">Username:</label><br>
+    <input type = "text" name = "username" required>
+
+    <label for = "password">Password:</label><br>
+    <input type = "password" name = "password" required>
+
+    <input type = "submit" value = "Sign in" id = "butn" class="btn btn-outline-primary">
+</form>
+
+<?php
     include("footer.php");
 ?>
